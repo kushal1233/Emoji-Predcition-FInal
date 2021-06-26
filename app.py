1c48584c7cb5aedad12f4df5874d1bbbe70e7f7f
kushal1233 <86479344+kushal1233@users.noreply.github.com>
2021-06-26 13:58:08 +0530

    Update and rename app12.py to app.py

--- app.py
+++ app.py
@@ -170,4 +170,6 @@ def index_post():
     url_out=np.random.choice(lines)
     return redirect("/")
  
-#app.run(debug=True,port=5000)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
